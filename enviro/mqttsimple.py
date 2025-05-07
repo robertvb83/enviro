ad19ec1df187ebf25c5b8c0c41a578d7a528c5c1
robertvb83 <36081149+robertvb83@users.noreply.github.com>
2025-05-07 21:28:45 +0200

    Update mqttsimple.py

--- enviro/mqttsimple.py
+++ enviro/mqttsimple.py
@@ -64,53 +64,72 @@ class MQTTClient:
 
     def connect(self, clean_session=True):
     #def connect(self, clean_session=True, timeout=30): # TODO this was added to 0.0.8
-        self.sock = socket.socket()
-        #self.sock.settimeout(timeout) # TODO this was added to 0.0.8
-        addr = socket.getaddrinfo(self.server, self.port)[0][-1]
-        self.sock.connect(addr)
-        if self.ssl:
-            import ussl
+        try:
+            self.sock = socket.socket()
+            #self.sock.settimeout(timeout) # TODO this was added to 0.0.8
+            addr = socket.getaddrinfo(self.server, self.port)[0][-1]
+            self.sock.connect(addr)
+            # RDebug output
+            print(f"TCP Connected to {self.server}:{self.port}")
+            ## RDebug
+            if self.ssl:
+                import ussl
 
-            self.sock = ussl.wrap_socket(self.sock, **self.ssl_params)
-        premsg = bytearray(b"\x10\0\0\0\0\0")
-        msg = bytearray(b"\x04MQTT\x04\x02\0\0")
+                self.sock = ussl.wrap_socket(self.sock, **self.ssl_params)
+            premsg = bytearray(b"\x10\0\0\0\0\0")
+            msg = bytearray(b"\x04MQTT\x04\x02\0\0")
 
-        sz = 10 + 2 + len(self.client_id)
-        msg[6] = clean_session << 1
-        if self.user is not None:
-            sz += 2 + len(self.user) + 2 + len(self.pswd)
-            msg[6] |= 0xC0
-        if self.keepalive:
-            assert self.keepalive < 65536
-            msg[7] |= self.keepalive >> 8
-            msg[8] |= self.keepalive & 0x00FF
-        if self.lw_topic:
-            sz += 2 + len(self.lw_topic) + 2 + len(self.lw_msg)
-            msg[6] |= 0x4 | (self.lw_qos & 0x1) << 3 | (self.lw_qos & 0x2) << 3
-            msg[6] |= self.lw_retain << 5
+            sz = 10 + 2 + len(self.client_id)
+            msg[6] = clean_session << 1
+            if self.user is not None:
+                sz += 2 + len(self.user) + 2 + len(self.pswd)
+                msg[6] |= 0xC0
+            if self.keepalive:
+                assert self.keepalive < 65536
+                msg[7] |= self.keepalive >> 8
+                msg[8] |= self.keepalive & 0x00FF
+            if self.lw_topic:
+                sz += 2 + len(self.lw_topic) + 2 + len(self.lw_msg)
+                msg[6] |= 0x4 | (self.lw_qos & 0x1) << 3 | (self.lw_qos & 0x2) << 3
+                msg[6] |= self.lw_retain << 5
 
-        i = 1
-        while sz > 0x7F:
-            premsg[i] = (sz & 0x7F) | 0x80
-            sz >>= 7
-            i += 1
-        premsg[i] = sz
+            i = 1
+            while sz > 0x7F:
+                premsg[i] = (sz & 0x7F) | 0x80
+                sz >>= 7
+                i += 1
+            premsg[i] = sz
 
-        self.sock.write(premsg, i + 2)
-        self.sock.write(msg)
-        # print(hex(len(msg)), hexlify(msg, ":"))
-        self._send_str(self.client_id)
-        if self.lw_topic:
-            self._send_str(self.lw_topic)
-            self._send_str(self.lw_msg)
-        if self.user is not None:
-            self._send_str(self.user)
-            self._send_str(self.pswd)
-        resp = self.sock.read(4)
-        assert resp[0] == 0x20 and resp[1] == 0x02
-        if resp[3] != 0:
-            raise MQTTException(resp[3])
-        return resp[2] & 1
+            self.sock.write(premsg, i + 2)
+            self.sock.write(msg)
+            # print(hex(len(msg)), hexlify(msg, ":"))
+            self._send_str(self.client_id)
+            if self.lw_topic:
+                self._send_str(self.lw_topic)
+                self._send_str(self.lw_msg)
+            if self.user is not None:
+                self._send_str(self.user)
+                self._send_str(self.pswd)
+            resp = self.sock.read(4)
+            if not resp:
+                raise MQTTException("No response from broker")
+                
+            print(f"MQTT Connection response: {hexlify(resp)}")
+            
+            if resp[0] != 0x20 or resp[1] != 0x02:
+                raise MQTTException("Invalid connection response")
+                
+            if resp[3] != 0:
+                raise MQTTException(f"Connection refused: {resp[3]}")
+                
+            print("MQTT Connected successfully")
+            return True
+        except Exception as e:
+            print(f"Connection failed: {e}")
+            if self.sock:
+                self.sock.close()
+            self.sock = None
+            raise
 
     def disconnect(self):
         self.sock.write(b"\xe0\0")
@@ -120,39 +139,46 @@ class MQTTClient:
         self.sock.write(b"\xc0\0")
 
     def publish(self, topic, msg, retain=False, qos=0):
-        pkt = bytearray(b"\x30\0\0\0")
-        pkt[0] |= qos << 1 | retain
-        sz = 2 + len(topic) + len(msg)
-        if qos > 0:
-            sz += 2
-        assert sz < 2097152
-        i = 1
-        while sz > 0x7F:
-            pkt[i] = (sz & 0x7F) | 0x80
-            sz >>= 7
-            i += 1
-        pkt[i] = sz
-        # print(hex(len(pkt)), hexlify(pkt, ":"))
-        self.sock.write(pkt, i + 1)
-        self._send_str(topic)
-        if qos > 0:
-            self.pid += 1
-            pid = self.pid
-            struct.pack_into("!H", pkt, 0, pid)
-            self.sock.write(pkt, 2)
-        self.sock.write(msg)
-        if qos == 1:
-            while 1:
-                op = self.wait_msg()
-                if op == 0x40:
-                    sz = self.sock.read(1)
-                    assert sz == b"\x02"
-                    rcv_pid = self.sock.read(2)
-                    rcv_pid = rcv_pid[0] << 8 | rcv_pid[1]
-                    if pid == rcv_pid:
-                        return
-        elif qos == 2:
-            assert 0
+        try:
+            pkt = bytearray(b"\x30\0\0\0")
+            pkt[0] |= qos << 1 | retain
+            sz = 2 + len(topic) + len(msg)
+            if qos > 0:
+                sz += 2
+            assert sz < 2097152
+            i = 1
+            while sz > 0x7F:
+                pkt[i] = (sz & 0x7F) | 0x80
+                sz >>= 7
+                i += 1
+            pkt[i] = sz
+            # print(hex(len(pkt)), hexlify(pkt, ":"))
+            self.sock.write(pkt, i + 1)
+            self._send_str(topic)
+            if qos > 0:
+                self.pid += 1
+                pid = self.pid
+                struct.pack_into("!H", pkt, 0, pid)
+                self.sock.write(pkt, 2)
+            self.sock.write(msg)
+            if qos == 1:
+                while 1:
+                    op = self.wait_msg()
+                    if op == 0x40:
+                        sz = self.sock.read(1)
+                        assert sz == b"\x02"
+                        rcv_pid = self.sock.read(2)
+                        rcv_pid = rcv_pid[0] << 8 | rcv_pid[1]
+                        if pid == rcv_pid:
+                            return
+            elif qos == 2:
+                assert 0
+            print(f"Published to {topic}: {msg}")
+            return True
+        except Exception as e:
+            print(f"Publish failed: {e}")
+            self.sock = None
+            raise
 
     def subscribe(self, topic, qos=0):
         assert self.cb is not None, "Subscribe callback is not set"
